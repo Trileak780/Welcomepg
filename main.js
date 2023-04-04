a89ac6a4703840468e0dd69202308af68c3bc72e
Trileak780 <129284946+Trileak780@users.noreply.github.com>
2023-04-04 12:27:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var text = document. querySelector('hm'). value;
+function yesf() {
+    document.getElementById("l1").innerHTML = "Yes";
+}
+function nof() {
+    document.getElementById("l1").innerHTML = "No";
+}
+function ifinf() {
+    document.getElementById("l2").innerHTML = " A lot of things! I can use functions, arrays, events, and much more!";
+}
+function show() {
+    document.getElementById("l3").innerHTML = "I love u! PLS DONT GO AWAY AGAIN!!! PWEASE!";
+}
+function hide() {
+    document.getElementById("l3").innerHTML = "";
+}
+function syke() {
+    document.getElementById("syke").innerHTML = "";
+    document.getElementById("l4").innerHTML = "3. HAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPY!!!";
+}
+function what() {
+    document.getElementById("syke").innerHTML = "3. HAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPYHAPPY!!!";
+    document.getElementById("l4").innerHTML = "";
+}
+function on()
+{
+    document.getElementById("youtube").play();
+}
+function off()
+{
+    stop();
+}
